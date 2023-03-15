dd0664ca9f448388edeb1643603fc948408beeed
Ana Freitas <111003166+anapaulafreita@users.noreply.github.com>
2023-03-15 14:35:08 -0300

    Add files via upload

---
+++ ExercicioAlunos/Alunos.cs
@@ -0,0 +1,30 @@
+namespace ExercicioAlunos;
+class Alunos
+{
+   public string nome="";
+   public double nota1, nota2;
+
+   //média
+   public double obterMedia(){
+    double media = (nota1+nota2)/2;
+    return media;
+   }
+
+   //situação
+   public string obterSituacao(double media){
+    string situacao="";
+    if(media>=6){
+        situacao="Aprovado!";
+    }else{
+        situacao="Reprovado";
+    }
+    return situacao;
+   }
+
+   //mensagem
+   public void mensagem(){
+    double mediaCalculada = obterMedia();
+    string resultadoSituacao = obterSituacao(mediaCalculada);
+    Console.WriteLine(nome+" está " +resultadoSituacao+" com média: "+mediaCalculada);
+   }
+}
