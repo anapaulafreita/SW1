0d5ac71e127497da5e2837d45438f0e5ca44087c
Ana Freitas <111003166+anapaulafreita@users.noreply.github.com>
2023-03-29 14:30:54 -0300

    Métodos acessores

---
+++ Metodos acessores/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace exemplo;
+class Produto
+{
+    private string? Nome {get; set;}
+    private double Preco {get; set;}
+
+    public void MostraDdos(){
+        Console.WriteLine("Nome: " + Nome);
+        Console.WriteLine("Preço: " + Preco);
+    }
+
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+
+    public void AlteraPreco(double preco){
+        this.Preco = preco;
+    }
+}
