84063ecfe4c835fc30024013a92c17b8c2bb8fe0
Ana Freitas <111003166+anapaulafreita@users.noreply.github.com>
2023-04-05 13:48:05 -0300

    Métodos construtores

---
+++ Metodos_contrutor/Produto.cs
@@ -0,0 +1,34 @@
+﻿namespace metodos_construtor;
+class Produto
+{
+    public Produto(){
+        this.Nome = "curupira";
+        this.Preco = 10;
+    }
+
+    public Produto(double preco){
+        this.Nome = "Não informado";
+        this.Preco = preco;
+    }
+
+    public Produto(string nome,double desconto){
+        this.Nome = nome;
+        this.Preco = 10-10*desconto/100;
+    }
+    
+    private string? Nome {get; set;}
+    private double Preco {get; set;}
+
+    public void MostraDados(){
+        Console.WriteLine("Nome: " + Nome);
+        Console.WriteLine("Preço: " + Preco);
+    }
+
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+
+    public void AlteraPreco(double preco){
+        this.Preco = preco;
+    }
+}
